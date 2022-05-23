556849006c0bb53d677fc0d4121f01a572cd39aa
MegaPush83 <hannespusch@arcor.de>
2022-05-23 16:22:48 +0200

    New SimpleAnalysis and new textprint of numberofvehicles per hour

--- src/main/java/org/matsim/analysis/SimpleAnalysis.java
+++ src/main/java/org/matsim/analysis/SimpleAnalysis.java
@@ -8,9 +8,20 @@ public class SimpleAnalysis {
 
         var handler = new SimplePersonEventHandler();
         var manager = EventsUtils.createEventsManager();
-        manager.addHandler(handler);
+        var linkVolumeHandler = new HourlyLinkVolumeHandler();
+        int numberofhours = 0;
+        double numberofvehicles = 0;
+
+       // manager.addHandler(handler);
+        manager.addHandler(linkVolumeHandler);
 
         EventsUtils.readEvents(manager, "/Users/hp/IdeaProjects/matsim-serengeti-park-hodenhagen/scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml.gz");
 
+        for (var hours : linkVolumeHandler.hourToVehicles.keySet()){
+            System.out.printf("%d: %d\n", hours, linkVolumeHandler.hourToVehicles.get(hours));
+            numberofhours++;
+            numberofvehicles = numberofvehicles + linkVolumeHandler.hourToVehicles.get(hours);
+        }
+        System.out.printf("Bei einer Gesamtzahl an Autos von %.0f und %d Stunden sind es im Schnitt:\n %.2f Autos pro Stunde", numberofvehicles, numberofhours, numberofvehicles / numberofhours);
     }
 }
